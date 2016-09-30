f59dd9c848eeea1a2ba44f7713d2f642a1de030b
Ana Jianu <ana.jianu@yahoo.com>
2016-09-30 22:00:09 +0300

    set GridBagConstraints properties to components
    at the moment, the components alignment needs improvement

--- src/ro/anajianu/agendatelefonica/view/PanouDetalii.java
+++ src/ro/anajianu/agendatelefonica/view/PanouDetalii.java
@@ -7,6 +7,7 @@ package ro.anajianu.agendatelefonica.view;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -16,13 +17,13 @@ import javax.swing.JTextField;
  * @author ana
  */
 public class PanouDetalii extends JPanel {
-    private JLabel pozaAbonat;
-    private JTextField numeAbonat;
-    private JTextField prenumeAbonat;
-    private JLabel telefonAbonatEticheta;
-    private JTextField telefonAbonatValoare;
-    private JLabel cnpAbonatEticheta;
-    private JTextField cnpAbonatValoare;
+    private final JLabel pozaAbonat;
+    private final JTextField numeAbonat;
+    private final JTextField prenumeAbonat;
+    private final JLabel telefonAbonatEticheta;
+    private final JTextField telefonAbonatValoare;
+    private final JLabel cnpAbonatEticheta;
+    private final JTextField cnpAbonatValoare;
     
     public PanouDetalii() {
         pozaAbonat=new JLabel("Poza");
@@ -41,30 +42,45 @@ public class PanouDetalii extends JPanel {
         
         gbc.gridx=0;
         gbc.gridy=0;
+        gbc.anchor=GridBagConstraints.NORTHWEST;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.insets=new Insets(20, 20, 0, 0);
         add(pozaAbonat, gbc);
         
         gbc.gridx=1;
         gbc.gridy=0;
+        gbc.ipadx=100;
         add(numeAbonat,gbc);
         
         gbc.gridx=2;
         gbc.gridy=0;
+        gbc.ipadx=100;
         add(prenumeAbonat,gbc);
         
         gbc.gridx=0;
         gbc.gridy=1;
+        gbc.ipadx=0;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.insets=new Insets(0, 0, 0, 0);
         add(telefonAbonatEticheta,gbc);
         
         gbc.gridx=1;
         gbc.gridy=1;
+        gbc.gridwidth=2;
+        gbc.ipadx=200;
         add(telefonAbonatValoare,gbc);
         
         gbc.gridx=0;
         gbc.gridy=2;
+        gbc.ipadx=0;
         add(cnpAbonatEticheta,gbc);
         
         gbc.gridx=1;
         gbc.gridy=2;
+        gbc.gridwidth=2;
+        gbc.ipadx=200;
         add(cnpAbonatValoare,gbc);
     }
     
