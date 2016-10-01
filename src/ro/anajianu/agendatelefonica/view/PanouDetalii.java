f1e1119be9456899c028628ca292ecdba612f622
Ana Jianu <ana.jianu@yahoo.com>
2016-10-01 19:40:46 +0300

    added an empty image icon

--- src/ro/anajianu/agendatelefonica/view/PanouDetalii.java
+++ src/ro/anajianu/agendatelefonica/view/PanouDetalii.java
@@ -7,7 +7,7 @@ package ro.anajianu.agendatelefonica.view;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -17,73 +17,65 @@ import javax.swing.JTextField;
  * @author ana
  */
 public class PanouDetalii extends JPanel {
+
     private final JLabel pozaAbonat;
+    private final ImageIcon iconAbonat;
     private final JTextField numeAbonat;
     private final JTextField prenumeAbonat;
     private final JLabel telefonAbonatEticheta;
     private final JTextField telefonAbonatValoare;
     private final JLabel cnpAbonatEticheta;
     private final JTextField cnpAbonatValoare;
-    
+
     public PanouDetalii() {
-        pozaAbonat=new JLabel("Poza");
-        numeAbonat=new JTextField();
-        prenumeAbonat=new JTextField();
-        telefonAbonatEticheta=new JLabel("Telefon:");
-        telefonAbonatValoare=new JTextField();
-        cnpAbonatEticheta=new JLabel("CNP:");
-        cnpAbonatValoare=new JTextField();
+        iconAbonat = new ImageIcon("resources/emptyPhoto.png");
+        pozaAbonat = new JLabel(iconAbonat);
+        numeAbonat = new JTextField();
+        prenumeAbonat = new JTextField();
+        telefonAbonatEticheta = new JLabel("Telefon:");
+        telefonAbonatValoare = new JTextField();
+        cnpAbonatEticheta = new JLabel("CNP:");
+        cnpAbonatValoare = new JTextField();
         initializare();
     }
 
     private void initializare() {
         setLayout(new GridBagLayout());
-        GridBagConstraints gbc=new GridBagConstraints();
-        
-        gbc.gridx=0;
-        gbc.gridy=0;
-        gbc.anchor=GridBagConstraints.NORTHWEST;
-        gbc.weightx = 1;
-        gbc.weighty = 1;
-        gbc.insets=new Insets(20, 20, 0, 0);
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
         add(pozaAbonat, gbc);
-        
-        gbc.gridx=1;
-        gbc.gridy=0;
-        gbc.ipadx=100;
-        add(numeAbonat,gbc);
-        
-        gbc.gridx=2;
-        gbc.gridy=0;
-        gbc.ipadx=100;
-        add(prenumeAbonat,gbc);
-        
-        gbc.gridx=0;
-        gbc.gridy=1;
-        gbc.ipadx=0;
-        gbc.weightx = 0;
-        gbc.weighty = 0;
-        gbc.insets=new Insets(0, 0, 0, 0);
-        add(telefonAbonatEticheta,gbc);
-        
-        gbc.gridx=1;
-        gbc.gridy=1;
-        gbc.gridwidth=2;
-        gbc.ipadx=200;
-        add(telefonAbonatValoare,gbc);
-        
-        gbc.gridx=0;
-        gbc.gridy=2;
-        gbc.ipadx=0;
-        add(cnpAbonatEticheta,gbc);
-        
-        gbc.gridx=1;
-        gbc.gridy=2;
-        gbc.gridwidth=2;
-        gbc.ipadx=200;
-        add(cnpAbonatValoare,gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.ipadx = 100;
+        add(numeAbonat, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.ipadx = 100;
+        add(prenumeAbonat, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.ipadx = 0;
+        add(telefonAbonatEticheta, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.ipadx = 200;
+        add(telefonAbonatValoare, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.ipadx = 0;
+        add(cnpAbonatEticheta, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.ipadx = 200;
+        add(cnpAbonatValoare, gbc);
     }
-    
-    
-    
+
 }
